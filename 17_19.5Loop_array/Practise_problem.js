16f9caf7eb3c636ff409a2ba23d278be039b3e66
AyshaUrmi <ayshaismail021@gmail.com>
2023-09-27 20:08:10 +0600

    commit

--- 17_19.5Loop_array/Practise_problem.js
+++ 17_19.5Loop_array/Practise_problem.js
@@ -3,3 +3,35 @@ var giverMoney=1000;
 var buyItem=700;
 var returnMoney=giverMoney-buyItem;
 console.log(returnMoney);
+
+
+// 2nd problem
+
+// Define the scores for you and your friends
+const yourScore = 85;
+const tomScore = 66;
+const janeScore = 95;
+const peterScore = 56;
+const johnScore = 40;
+
+// Function to calculate grades
+function calculateGrade(score) {
+  if (score >= 80) {
+    return 'A grade';
+  } else if (score >= 60) {
+    return 'B grade';
+  } else if (score >= 50) {
+    return 'C grade';
+  } else if (score >= 40) {
+    return 'D grade';
+  } else {
+    return 'F grade';
+  }
+}
+
+// Calculate and display grades
+console.log(yourScore);
+console.log(tomScore);
+console.log(janeScore);
+console.log(peterScore);
+console.log(`John's grade: ${calculateGrade(johnScore)}`);
--- 17_19.5Loop_array/fruits.js
+++ 17_19.5Loop_array/fruits.js
@@ -1,24 +1,67 @@
 var fruits=['apple','banana','mango','orange','grapes','papaya'];
-var orangeIndex=fruits.indexOf('orange');
+var orangeIndex=fruits.indexOf('orange');//indexof means it will return the index of the element
 console.log(orangeIndex);
 console.log(fruits);
-fruits.push('watermelon');
+fruits.push('watermelon');//push means it will add the element at the end of the array
 console.log(fruits);
-fruits.pop();
+fruits.pop();//pop means it will remove the last element
 console.log(fruits);
-fruits.shift();
+fruits.shift();//shift means it will remove the first element
 console.log(fruits);
-fruits.unshift('lemon');
+fruits.unshift('lemon');//unshift means it will add the element at the beginning of the array
 console.log(fruits);
-var orangeIndex=fruits.indexOf('orange');
+var orangeIndex=fruits.indexOf('orange');//indexof means it will return the index of the element
 console.log(orangeIndex);
 fruits[1] ='watermelon';
 console.log(fruits);
-fruits.slice(1,3);
+fruits.slice(1,3);//slice means it will remove the elements of the array
 console.log(fruits);
-fruits.includes('orange');
+fruits.includes('orange');//includes means it will return true or false
 console.log(fruits);
-fruits.substring(1,3);
+fruits.substring(1,3);//substring means it will return the elements of the array
 console.log(fruits);
+
+//concate
+var firstArray=['a','b','c','d'];
+var secondArray=['e','f','g','h'];
+var thirdArray=firstArray.concat(secondArray);//concat means it will join the elements of the array
+console.log(thirdArray);
+
+//forEach:forEach means it will return the elements of the array
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+fruits.forEach(function(element){
+    console.log(element);
+
+})
+//map:map means it will return the elements of the array
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+var newFruits=fruits.map(function(element){
+    
+    return element;
+})
+console.log(newFruits);
+//filter:filter means it will return the elements of the array
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+var newFruits=fruits.filter(function(element){
+    
+    return element;
+})
+console.log(newFruits);
+//reduce:reduce means it will return the elements of the array
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+var newFruits=fruits.reduce(function(element){
+    
+    return element;
+})
+console.log(newFruits);
+//sort:
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+var newFruits=fruits.sort();
+console.log(newFruits);
+
+//reverse:
+var fruits=['apple','banana','mango','orange','grapes','papaya'];
+var newFruits=fruits.reverse();
+console.log(newFruits);
 
 
